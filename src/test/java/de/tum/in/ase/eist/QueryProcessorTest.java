47ba99e280dad74a8dc75463c685ff2205f5eb0e
tanbier <tanbier.ahmed@gmail.com>
2022-07-15 11:45:41 +0200

    add two numbers with test

--- src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
+++ src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
@@ -30,4 +30,15 @@ class QueryProcessorTest {
 		}
 	}
 
+	@Test
+	void testString() {
+		String actual = "%20what%20is%204%20plus%207";
+		String[] split = actual.split("%20");
+		String sum1 = split[3];
+		int summand1 = Integer.parseInt(sum1);
+		String sum2 = split[5];
+		int summand2 = Integer.parseInt(sum2);
+		assertEquals(11, summand1 + summand2);
+	}
+
 }
